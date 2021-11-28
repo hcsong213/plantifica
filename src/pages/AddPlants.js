ea790fd70284e001cb5e3e7c877acc87da0a0bf3
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-11-27 23:37:41 -0800

    Add SideMarginLayout
    Added layout component which wraps its children in a div that has
    mx-5. See search page for an example of usage.

--- src/pages/AddPlants.js
+++ src/pages/AddPlants.js
@@ -1,21 +1,24 @@
 import CustomNavbar from "../components/CustomNavbar";
 import Form from "react-bootstrap/Form";
+import SideMarginLayout from "../layouts/SideMarginLayout";
 import Button from "react-bootstrap/Button";
 
 function AddPlants() {
   return (
     <div>
       <CustomNavbar />
-      <h1>This is the Plant Search Page!</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="PlantName">
-          <Form.Label>Plant name</Form.Label>
-          <Form.Control type="text" placeholder="Enter a plant name" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <SideMarginLayout>
+        <h1>This is the Plant Search Page!</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="PlantName">
+            <Form.Label>Plant name</Form.Label>
+            <Form.Control type="text" placeholder="Enter a plant name" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </SideMarginLayout>
     </div>
   );
 }
