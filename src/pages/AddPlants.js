f144dcad4515fcbd4dd954251e53c63a83939817
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2021-11-27 22:53:41 -0800

    search page + some styling

--- src/pages/AddPlants.js
+++ src/pages/AddPlants.js
@@ -1,12 +1,23 @@
 import CustomNavbar from "../components/CustomNavbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function AddPlants() {
-    return (
-        <div>
-            <CustomNavbar/>
-            <h1>This is the Plant Search Page!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <CustomNavbar />
+      <h1>This is the Plant Search Page!</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="PlantName">
+          <Form.Label>Plant name</Form.Label>
+          <Form.Control type="text" placeholder="Enter a plant name" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default AddPlants;
