6c28f81f2aeb623124d3fab416d973b9a54c2722
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2021-11-27 23:33:45 -0800

    linking on home to login page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import CustomNavbar from "../components/CustomNavbar";
+import { Link } from 'react-router-dom';
 
 function Home() {
 
@@ -15,7 +16,7 @@ function Home() {
             <Container fluid>
                 <Row className="justify-content-md-center align-me" style={divStyle}>
                     <Col md="auto">
-                        <h1>plantifica</h1>
+                        <h1>welcome to plantifica!</h1>
                     </Col>
                 </Row>
                 <Row className="justify-content-md-center align-me">
@@ -25,6 +26,34 @@ function Home() {
                 </Row>
                 <Row className="justify-content-md-center align-me">
                     <Col md="auto">
+                        <p>By creating a Plantifica account, you can: 
+                        </p>
+                        <ul>
+                            <li>Track the plants you own</li>
+                            <li>Learn about their characteristics</li>
+                            <li>Win achievements for your plant collection</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center align-me">
+                    <Col md="auto">
+                        <h3>Ready to get started?</h3>
+                        <h4>Make an account or login below:</h4>
+                    </Col>
+                </Row>
+                <br/>
+                <Row className="justify-content-md-center align-me">
+                    <Col md="auto">
+                        {/* link below to the account creation page*/}
+                    <Link to =""><h4>Create an Account</h4></Link>
+                    </Col>
+                    <Col md="auto">
+                    <Link to ="/login"><h4>Login</h4></Link>
+                    </Col>
+                </Row>
+                <br/>
+                <Row className="justify-content-md-center align-me">
+                    <Col md="auto">
                         <Button variant='primary' size='lg' href='/profile'>Your Plants</Button>
                     </Col>
                     <Col md="auto">
@@ -32,7 +61,7 @@ function Home() {
                     </Col>
                 </Row>
             </Container>
-
+           
         </div>
     )
 }
