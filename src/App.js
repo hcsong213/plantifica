1b2de6779e40cd29c9e939ad190970eb5ff61d4d
Nikhil Suresh <niksure24@g.ucla.edu>
2021-11-27 20:57:18 -0800

    routing now depends on auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Landing from './pages/Landing';
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
+import { getAuth } from '@firebase/auth';
+import Landing from './pages/Landing';
 import Account from './pages/Account';
 import AddPlants from './pages/AddPlants';
 import Achievements from './pages/Achievements';
@@ -12,18 +13,30 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 
+function requireAuth(nextState, replace, next) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (!user) {
+    replace({
+      pathname: "/login",
+      state: {nextPathname: nextState.location.pathname}
+    });
+  }
+  next();
+}
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* TODO: Route to Landing if not logged in */}
-          <Route path="/" element={<Home />} />
-          <Route path="new-account" element={<Account />} />
-          <Route path="achievements" element={<Achievements />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="search" element={<AddPlants />} />
+          <Route path="/" element={<Home />} onEnter={requireAuth} />
+          <Route path="new-account" element={<Account />} onEnter={requireAuth} />
+          <Route path="achievements" element={<Achievements />} onEnter={requireAuth} />
+          <Route path="profile" element={<Profile />} onEnter={requireAuth} />
+          <Route path="settings" element={<Settings />} onEnter={requireAuth} />
+          <Route path="search" element={<AddPlants />} onEnter={requireAuth} />
+          <Route path="login" element={<Landing />} />
         </Routes>
       </BrowserRouter>
     </div>
