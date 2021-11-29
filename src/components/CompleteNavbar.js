c138fefa10f5408185eaab2b08ee96ff927ff32c
Nikhil Suresh <niksure24@g.ucla.edu>
2021-11-28 23:02:21 -0800

    CompleteNavbar loads CustomNavbar when user logged in

---
+++ src/components/CompleteNavbar.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { getAuth } from "firebase/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import CustomNavBar from './CustomNavbar.js';
+import LoginNavbar from './LoginNavbar.js';
+
+function CompleteNavbar(props) {
+    const auth = getAuth();
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (loading) {
+            return;
+        }
+    }, [user, loading]);
+
+    return (
+        <div>
+            {user ? <CustomNavBar /> : <LoginNavbar />}
+        </div>
+    )
+
+}
+
+export default CompleteNavbar;
