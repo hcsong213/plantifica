0ad6ab7476e8029b49a59fdea4d453b8ecda1722
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-11-28 16:29:42 -0800

    Format with prettier
    VSCode extension: https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
    CLI: https://levelup.gitconnected.com/how-to-format-all-files-in-a-directory-with-prettier-5f0ff5f4ffb2

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,12 +1,12 @@
 import CustomNavbar from "../components/CustomNavbar";
 
 function Account() {
-    return (
-        <div>
-            <CustomNavbar/>
-            <h1>This is the New Account Page!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <CustomNavbar />
+      <h1>This is the New Account Page!</h1>
+    </div>
+  );
 }
 
 export default Account;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,55 +1,62 @@
 import CustomNavbar from "../components/CustomNavbar";
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from 'react-bootstrap';
-import Avatar from 'react-avatar';
+import { Col, Container, Row } from "react-bootstrap";
+import Avatar from "react-avatar";
 
 function Profile() {
-    var divStyle = {
-        padding: "20px",
-        margin: "20px"
-    };
+  var divStyle = {
+    padding: "20px",
+    margin: "20px",
+  };
 
-    return (
-        <div>
-            <CustomNavbar />
-            <Container fluid>
-                <Row className="justify-content-md-center align-me" style={divStyle}>
-                    <Col md="auto">
-                        <h1>my profile</h1>
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-center align-me">
-                    <Col md="auto">
-                        <Avatar color={'darkseagreen'} name=" Dummy Name" size="200" round={true} />
-                    </Col>
-                </Row>
-                <Row className="d-flex justify-content-center" style={ {
-                    margin: "10px"
-                }}>
-                    <Col md="auto" >
-                        <h2>username</h2>
-                    </Col>
-                </Row>
-                <Row className="d-flex justify-content-center" style={ {
-                    margin: "10px"
-                }} > 
-                    <Col md="auto" >
-                        <h2>email</h2>
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-center align-me" style={divStyle}>
-                    <Col md="auto">
-                        <h1>achievements</h1>
-                    </Col>
-                </Row>
-            </Container>
-            <div className="profile">
-
-
-            </div>
-        </div>
-
-    );
+  return (
+    <div>
+      <CustomNavbar />
+      <Container fluid>
+        <Row className="justify-content-md-center align-me" style={divStyle}>
+          <Col md="auto">
+            <h1>my profile</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center align-me">
+          <Col md="auto">
+            <Avatar
+              color={"darkseagreen"}
+              name=" Dummy Name"
+              size="200"
+              round={true}
+            />
+          </Col>
+        </Row>
+        <Row
+          className="d-flex justify-content-center"
+          style={{
+            margin: "10px",
+          }}
+        >
+          <Col md="auto">
+            <h2>username</h2>
+          </Col>
+        </Row>
+        <Row
+          className="d-flex justify-content-center"
+          style={{
+            margin: "10px",
+          }}
+        >
+          <Col md="auto">
+            <h2>email</h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center align-me" style={divStyle}>
+          <Col md="auto">
+            <h1>achievements</h1>
+          </Col>
+        </Row>
+      </Container>
+      <div className="profile"></div>
+    </div>
+  );
 }
 
 export default Profile;
